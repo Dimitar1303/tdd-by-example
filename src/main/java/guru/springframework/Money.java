137dea63d9ac39db788d31efa48b220a183cb7d9
dimitar.josheski@outlook.com <dimitar.josheski@outlook.com>
2020-10-30 15:17:58 +0100

    Making objects

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -2,8 +2,17 @@ package guru.springframework;
 
 import java.util.Objects;
 
-public class Money {
+public abstract class Money {
     protected int amount;
+
+    public abstract Money times(int multiplier);
+
+    public static Dollar dollar(int amount){
+        return new Dollar(amount);
+    }
+    public  static Franc franc(int amount){
+        return new Franc(amount);
+    }
 
     @Override
     public boolean equals(Object o) {
